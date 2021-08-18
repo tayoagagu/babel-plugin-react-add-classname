61c2578ed76f727a8c36e59bac50ded84f2c0c58
Tayo Agagu <codename85@gmail.com>
2021-08-18 14:48:09 +0200

    add gitgnore for lib and coverage

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PluginObj } from '@babel/core';
-interface State {
-    opts: {
-        name?: string;
-    };
-}
-declare const _default: () => PluginObj<State>;
-export default _default;
