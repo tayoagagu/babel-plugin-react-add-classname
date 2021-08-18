076d44f387a6cb46b2fda291f563b27f85a63b4e
Tayo Agagu <codename85@gmail.com>
2021-08-18 14:37:38 +0200

    implement babel-plugin-react-add-classname

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+import { PluginObj } from '@babel/core';
+interface State {
+    opts: {
+        name?: string;
+    };
+}
+declare const _default: () => PluginObj<State>;
+export default _default;
